429071b3452280085612c379fd041eb1d0f7299d
DS13 <starkishore2@gmail.com>
2020-12-16 12:26:45 +0530

    finished sampleFramework

---
+++ sampleFramework/sampleFramework/additionService.swift
@@ -0,0 +1,15 @@
+//
+//  additionService.swift
+//  sampleFramework
+//
+//  Created by Kishore S on 16/12/20.
+//
+
+import Foundation
+public class additionService{
+    private init(){}
+    
+    public static func addTwoNumber(num1: Int , num2: Int) -> Int{
+        return num1 + num2
+    }
+}
---
+++ sampleFramework/sampleFramework/sampleFramework.h
@@ -0,0 +1,18 @@
+//
+//  sampleFramework.h
+//  sampleFramework
+//
+//  Created by Kishore S on 16/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sampleFramework.
+FOUNDATION_EXPORT double sampleFrameworkVersionNumber;
+
+//! Project version string for sampleFramework.
+FOUNDATION_EXPORT const unsigned char sampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sampleFramework/PublicHeader.h>
+
+
